5d803f99442532653b98fcf233af162d571edc41
ScottGrogin <israelscottgrogin@gmail.com>
2020-08-04 12:15:14 -0700

    Added Logout.

--- src/main/java/io/github/scottgrogin/TaskTeam/Controllers/LoginController.java
+++ src/main/java/io/github/scottgrogin/TaskTeam/Controllers/LoginController.java
@@ -1,7 +1,6 @@
 package io.github.scottgrogin.TaskTeam.Controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
